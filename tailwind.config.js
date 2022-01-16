fb54ceadbaf652805d9165471fbefd4adfbdf0c2
Akhmedov Farkhod <a.farkhod@gmail.com>
2022-01-16 23:12:50 +0500

    Organic Landing page using Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        green: {
+          DEFAULT: '#71B214',
+        },
+        gray: {
+          600: '#606060',
+        }
+      },
+      spacing: {
+        13: '3.25rem',
+      },
+      boxShadow: {
+        primary: '0px 9.9px 21.6px rgba(136, 202, 41, 0.41)'
+      }
+    },
   },
   plugins: [],
 }
